2f3d47c394ccec98219da3803598541b2a041c0a
Reza.Abbaszadeh <reza.abbaszadeh@partsoftware.com>
2022-03-24 21:26:58 +0430

    get course data

--- YU.py
+++ YU.py
@@ -1,4 +1,4 @@
-import BaseCrawler
+from BaseCrawler import BaseCrawler
 import requests
 from bs4 import BeautifulSoup
 import logging
@@ -6,28 +6,28 @@ import logging
 logger = logging.getLogger('__main__')
 
 
-class YU():
+class UOY(BaseCrawler):
     Department_Page_Url = "https://www.york.ac.uk/students/studying/manage/programmes/module-catalogue/module"
     University = "University of York"
-    Abbreviation = "UCB"
+    Abbreviation = "UOY"
     University_Homepage = "https://www.york.ac.uk/"
 
     # Below fields didn't find in the website
-    Prerequisite = None
-    References = None
     Scores = None
     Projects = None
+    Professor = None
     Professor_Homepage = None
+    Required_Skills = None
 
     def get_courses_of_department(self, department_id):
 
         academic_year = "2021-22"
 
-        department_url = self.Department_Page_Url + "?query=&department= " + department_id + "&year=" + academic_year
+        department_url = self.Department_Page_Url + "?query=&department=" + department_id + "&year=" + academic_year
 
         department_course_len = self.get_courseList_len(department_url)
 
-        department_url += "&max=" + department_course_len + "&offset=0"
+        department_url += "&max=" + str(department_course_len) + "&offset=0"
 
         department_page_content = requests.get(department_url).text
         department_soup = BeautifulSoup(department_page_content, 'html.parser')
@@ -50,45 +50,53 @@ class YU():
         department_records_len = department_soup.find('div', {'class': 'pagination'}).find_all('a', {'class': 'step'})[
             -1].text
 
-        return department_records_len
+        return int(department_records_len) * 10
 
     def get_course_data(self, course):
-        Course_Title = course.find(class_="title").text
+        page_title = course.find(id="mdcolumn").find('h1').text
+        Course_Title = page_title
+        # Course_Title = page_title.rpartition(' - ')[0]
 
-        Unit_Count = course.find(class_="hours").text
-        Unit_Count = Unit_Count[:-5].rstrip()
+        Unit_Count = course.find('strong',text='Credit value').next_sibling.text
+        Unit_Count = Unit_Count.split(' ')[1]
 
-        Description = course.find(class_='courseblockdesc').text
 
-        course_sections = course.find_all(class_='course-section')
+        course_sections = course.find(id='mdcolumn').find_all('h2')
 
-        Objective = None
-        Outcome = None
-        Professor = None
-        Required_Skills = None
+        Prerequisite = None
+        pre_list = course.find('h3', text='Pre-requisite modules')
+        if pre_list is not None:
+            Prerequisite = pre_list.next_sibling.next_sibling.get_text().strip()
+
+        Description = ''
+        Objective = ''
+        Outcome = ''
+        References = ''
 
         for section in course_sections:
-            inner_sections = section.find_all('p')
-            for inner_section in inner_sections:
-                inner_section_title = inner_section.find('strong')
 
-                if inner_section_title.text == "Course Objectives:":
-                    inner_section_title.decompose()
-                    Objective = inner_section.text.strip()
+            if section.text == "Module summary":
+                summary = course.find('h2', text='Module summary')
+                if summary is not None:
+                    Description = self.remove_extra_newlines(summary.next_sibling.next_sibling.text)
+            
+            elif section.text == "Module content":
+                content = course.find('h2', text='Module content')
+                if content is not None:
+                    Description += '\n' + self.remove_extra_newlines(content.next_sibling.next_sibling.get_text())
 
-                if inner_section_title.text == "Student Learning Outcomes:":
-                    inner_section_title.decompose()
-                    Outcome = inner_section.text.strip()
+            elif section.text == "Module aims":
+                sub_aim = section.next_sibling.next_sibling.get_text()
+                Objective = self.remove_extra_newlines(sub_aim).strip()
 
-                if inner_section_title.text == "Instructor:":
-                    inner_section_title.decompose()
-                    Professor = inner_section.text.strip()
+            elif section.text == "Module learning outcomes":
+                sub_outcome = section.next_sibling.next_sibling.get_text()
+                Outcome = self.remove_extra_newlines(sub_outcome).strip()
 
-                if inner_section_title.text == "Prerequisites:":
-                    inner_section_title.decompose()
-                    Required_Skills = inner_section.text.strip()
+            if section.text == "Indicative reading":
+                References = section.next_sibling.next_sibling.get_text()
 
-        return Course_Title, Unit_Count, Objective, Outcome, Professor, Required_Skills, Description
+        return Course_Title, Unit_Count, Objective, Outcome, Prerequisite, Description, References
 
     def handler(self):
         html_content = requests.get(self.Department_Page_Url).text
@@ -106,29 +114,38 @@ class YU():
                 departments_with_id[child.text] = child.attrs['value']
 
         # get department's course
-
-        for department in departments_with_id:
-            department_id = departments_with_id[department]
-            Department_Name = department
+        for Department_Name in departments_with_id:
+            # department = 'Computer Science'
+            department_id = departments_with_id[Department_Name]
 
             courses, department_url = self.get_courses_of_department(department_id)
-            Course_Homepage = department_url
-            # get course's data
-            for course in courses:
-                Course_Title, Unit_Count, Objective, Outcome, Professor, Required_Skills, Description = self.get_course_data(
-                    course)
+            if courses is not None:
+                # get course's data
+                for course in courses:
+                    Course_Homepage = self.University_Homepage + course.find_all("a", href=True)[0]['href'][1:]
+                    course_page_content = requests.get(Course_Homepage).text
+                    course_soup = BeautifulSoup(course_page_content, 'html.parser')
+                    Course_Title, Unit_Count, Objective, Outcome, Prerequisite, Description, References = \
+                        self.get_course_data(course_soup)
 
-                self.save_course_data(
-                    self.University, self.Abbreviation, Department_Name, Course_Title, Unit_Count,
-                    Professor, Objective, self.Prerequisite, Required_Skills, Outcome, self.References, self.Scores,
-                    Description, self.Projects, self.University_Homepage, Course_Homepage, self.Professor_Homepage
-                )
+                    self.save_course_data(
+                        self.University, self.Abbreviation, Department_Name, Course_Title, Unit_Count,
+                        self.Professor, Objective, Prerequisite, self.Required_Skills, Outcome, References, self.Scores,
+                        Description, self.Projects, self.University_Homepage, Course_Homepage, self.Professor_Homepage
+                    )
+                    if self.course_count % 10 == 0:
+                        print(self.course_count)
 
-            logger.info(f"{self.Abbreviation}: {Department_Name} department's data was crawled successfully.")
+            print(f"{self.Abbreviation}: {Department_Name} department's data was crawled successfully.")
 
-            logger.info(f"{self.Abbreviation}: Total {self.course_count} courses were crawled successfully.")
+        print(f"{self.Abbreviation}: Total {self.course_count} courses were crawled successfully.")
+
+    def remove_extra_newlines(self, str):
+        cleaned_str = str
+        while '\n\n' in cleaned_str:
+            cleaned_str = cleaned_str.replace("\n\n", "\n")
+        return cleaned_str
 
 
-y = YU()
-
+y = UOY()
 y.handler()
